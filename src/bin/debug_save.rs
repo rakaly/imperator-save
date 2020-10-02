7020c16b4a4c48b86853ed43d258bc6d29faf796
Nick Babcock <nbabcock19@hotmail.com>
2020-10-02 05:31:07 -0500

    Update to jomini 0.7

--- src/bin/debug_save.rs
+++ src/bin/debug_save.rs
@@ -5,6 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let data = std::fs::read(&args[1])?;
     let (save, _encoding) = ImperatorExtractor::extract_header(&data[..])?;
-    print!("{:#?}", save.date.imperator_fmt());
+    print!("{:#?}", save.date.game_fmt());
     Ok(())
 }
