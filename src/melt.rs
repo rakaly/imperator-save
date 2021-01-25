c9630c49b9cdecbbff8443e0aa79e7df7b04fa0a
Nick Babcock <nbabcock19@hotmail.com>
2021-01-25 05:59:02 -0600

    Rustfmt

--- src/melt.rs
+++ src/melt.rs
@@ -153,7 +153,7 @@ impl Melter {
                     Some(id) => {
                         known_number = in_object == 1 && id == "seed";
                         writer.extend_from_slice(&id.as_bytes())
-                    },
+                    }
                     None => match self.on_failed_resolve {
                         FailedResolveStrategy::Error => {
                             return Err(ImperatorErrorKind::UnknownToken { token_id: *x }.into());
